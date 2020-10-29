e7530067c8c4d190605888080e649de665703c2a
aluno10 <aluno10@aluno10.tecnisys.com.br>
2020-10-29 16:02:04 -0300

    Corrigindo o texto da mensagem

--- src/main/java/br/com/tecnisys/java8/Endpoint.java
+++ src/main/java/br/com/tecnisys/java8/Endpoint.java
@@ -8,6 +8,6 @@ public class Endpoint {
 
     @GetMapping
     public String versao(){
-        return "Versão do Java"+System.getProperty("java.runtime.version");	
+        return "Versão do Java: "+System.getProperty("java.runtime.version");	
     }
 }
